9de6f779e78694e540456ac91040747c70ce2458
RichardHpa <richard.hpa@hotmail.com>
2018-08-21 21:09:52 +1200

    added task files

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response){
     console.log(`${request.method} request for ${request.url}`);
     var page;
 
-    if(request.url === "/" || request.url === "home" || request.url === "index"){
+    if(request.url === "/" || request.url === "/home" || request.url === "/index"){
         page = "home";
         fs.readFile('./public/index.html', 'UTF-8', function(error, contents){
             if(error){
@@ -17,12 +17,55 @@ var server = http.createServer(function(request, response){
                 response.end(contents);
             }
         });
+    } else if(request.url === "/about"){
+        fs.readFile('./public/about.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log("error, something went wrong");
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url === "/contact"){
+        fs.readFile('./public/contact.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log("error, something went wrong");
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url === "/news"){
+        fs.readFile('./public/news.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log("error, something went wrong");
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url.match(/.js/)){
+        var jsPath = path.join(__dirname, 'public', request.url);
+        var fileStream = fs.createReadStream(jsPath, 'UTF-8');
+        response.writeHead(200, {'Content-Type': 'text/javascript'});
+        fileStream.pipe(response);
     } else if(request.url.match(/.css$/)){
         var cssPath = path.join(__dirname, 'public', request.url);
         var fileStream = fs.createReadStream(cssPath, 'UTF-8');
         response.writeHead(200, {'Content-Type': 'text/css'});
         fileStream.pipe(response);
-    }
+    } else if(request.url.match(/.jpg$/)){
+		var imagePath = path.join(__dirname, 'public', request.url);
+		var imageStream = fs.createReadStream(imagePath);
+		response.writeHead(200, {'Content-Type':'image/jpeg'});
+		imageStream.pipe(response);
+	} else if(request.url.match(/.png/)){
+		var imagePath = path.join(__dirname, 'public', request.url);
+		var imageStream = fs.createReadStream(imagePath);
+		response.writeHead(200, {'Content-Type':'image/png'});
+		imageStream.pipe(response);
+	}
+
     // else if(request.url === "/contact"){
     //     page = "contact";
     // } else if(request.url === "/about"){
